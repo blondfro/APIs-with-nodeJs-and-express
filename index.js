5de4a395e9da1efef55c35628c8d9983f767fa8a
James Morris <blondfro@gmail.com>
2020-02-22 12:44:08 -0600

    installed body-parser and nodemon and setup a start script, index.js for the server.

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = 4000
+
+;
+
+app.get("/", (req, res) => {
+    res.send("Node and express server running on port: " + PORT);
+});
+
+app.listen(PORT, ()=> {
+    console.log("Server is running on port: " + PORT);
+});
